2bf35febf36b2dafe420206c95c40aeedcdce8be
Signior-X <b19188@students.iitmandi.ac.in>
2020-08-08 16:17:46 +0530

    Extension made

---
+++ README.md
@@ -0,0 +1,2 @@
+# V-speedo
+A chrome extension to change the playback speed of a video playing in a tab.
---
+++ popup.js
@@ -0,0 +1,13 @@
+try {
+    document.getElementById('speed-select').onchange = function() {
+        console.log("Speed changed");
+        let value = document.getElementById('speed-select').value;
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+                {code: 'document.getElementsByTagName("video")[0].playbackRate='+value+';'});
+          });
+    }
+} catch(e) {
+    alert('Speed change Failed!');
+}
